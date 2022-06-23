dfb8d0e63bbfebd709c05539e2d337b0cacab754
manonaidinian <manonaidinian@gmail.com>
2022-06-23 22:47:02 +0800

    fixing double migration

--- db/migrate/20220623120810_add_infostoo_users.rb
+++
@@ -1,7 +0,0 @@
-class AddInfostooUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
-    add_column :users, :type, :string
-  end
-end
