33a3c3922d7bc996976a80dbcb9fbe11fd78a2f1
manonaidinian <manonaidinian@gmail.com>
2022-06-23 22:14:48 +0800

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+Charity.destroy_all
+Company.destroy_all
+Need.destroy_all
+
+company = Company.new(email: "co@mail.com", password: "123123", name: "Company.ltd", location: Faker::Address.city)
+
+10.times do
+  charity = Charity.new(
+  name: Faker::Name.name,
+  location: Faker::Address.city,
+  description: Faker::Lorem.paragraph(sentence_count: 2)
+  )
+  charity.save!
+  5.times do
+    need = Need.new(
+    title: Faker::Lorem.sentence(word_count: 3),
+    karma_points: rand(1..50),
+    category: Need::CATEGORY.sample,
+    charity_id: charity.id
+  )
+  need.save!
+  end
+end
+
+p Charity.count
+p Need.count
