6f7d41c93aa54acc856d4b890fe413f55ab99ced
manonaidinian <manonaidinian@gmail.com>
2022-07-01 14:46:36 +0800

    setting up pgsearch

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -5,4 +5,11 @@ class Charity < ApplicationRecord
   validates :name, :description, :location, presence: true
 
   has_one_attached :photo
+
+  # include PgSearch::Model
+  # pg_search_scope :search_by_name_and_category_and_location,
+  #   against: [ :name, :category, :location ],
+  #   using: {
+  #     tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  #   }
 end
