d4b59bac342dce861f0cf3f3e5c186192f50b50f
manonaidinian <manonaidinian@gmail.com>
2022-07-22 16:19:51 +0800

    claiming process finish!

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -14,14 +14,25 @@ class ClaimsController < ApplicationController
     @need = Need.find(params[:need_id])
     @company = current_user.company
     @claim = Claim.new(need_id: @need.id, company_id: @company.id)
-    @claim.status = "On process"
-    @need.status = "Claimed and on process!"
+    @claim.status = "Waiting for approval"
+    @need.status = "Claimed and waiting for approval"
     @claim.save! && @need.save!
     redirect_to claim_path(@claim.id)
   end
 
-  def update
-    @claim = Claim.find(params[:id])
+  def approve
+    @claim = Claim.find(params[:claim_id])
+    @claim.status = "Approved"
+    @need = @claim.need
+    @claim.save!
+    @need.status = "Claimed"
+    @need.save!
+    @company = @claim.company
+    redirect_to claim_path(@claim.id)
+  end
+
+  def close
+    @claim = Claim.find(params[:claim_id])
     @claim.status = "Processed and Rewarded"
     @need = @claim.need
     @need.status = "Solved"
